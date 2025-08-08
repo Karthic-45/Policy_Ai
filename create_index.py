be677d8208975b502d0f78f0ffac2cba5e7a1c8f
karthic.N <karthicnandakumar4545@gmail.com>
2025-08-08 15:37:34 +0530

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -22,7 +22,8 @@ import requests
 
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 # Read OPENAI key from env (ensure .env has it)
